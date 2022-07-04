cc63f576943811aaf8f598e0289c1be5b000e08b
SKLC1 <segevklein2000@gmail.com>
2022-07-04 19:47:19 +0300

    explore

--- client/src/components/explorePage/explorepage.jsx
+++ client/src/components/explorePage/explorepage.jsx
@@ -1,6 +1,7 @@
 import axios from "axios"
 import { useState } from "react"
 import { useEffect } from "react"
+import JobCard from "../resuableComponents/jobCard/jobCard.jsx"
 
 
 function ExplorePage(){
@@ -13,15 +14,19 @@ function ExplorePage(){
   async function showJobsToUser(){
     const {data} = await axios.get('http://localhost:5000/jobs')
     setCards(data)
+    console.log(cards);
   }
-  const renderCards = cards.map((card)=>{
-    return;
-  })
+  function renderCards(){
+    return cards.map((card)=>{
+      console.log(card);
+      return <JobCard card={card} key={card._id}/>;
+    })
+  }
   
   return(
     <>
       <div>
-        
+        {renderCards()}
       </div>
     </>
   )
---
+++ client/src/components/resuableComponents/jobCard/jobCard.jsx
@@ -0,0 +1,13 @@
+
+
+function JobCard({card}){
+
+  return(
+    <>
+     <div
+     
+     >{card.company}</div>
+    </>
+  )
+}
+export default JobCard
