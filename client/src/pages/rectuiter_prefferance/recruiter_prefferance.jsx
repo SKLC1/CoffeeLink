72629c20ab8552866519fdf819f506f08a6ad1b3
SKLC1 <segevklein2000@gmail.com>
2022-07-08 16:44:28 +0300

    edit job pref

--- client/src/pages/rectuiter_prefferance/recruiter_prefferance.jsx
+++ client/src/pages/rectuiter_prefferance/recruiter_prefferance.jsx
@@ -2,14 +2,13 @@ import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 
 function RecruiterPreference({currentUser}) {
   const {job} = useParams()
   const [jobDetails, setJobDetails] = useState({})
   const [preferences, setPreferences] = useState([])
 
-  // const preferences = ['name','education','experience','skills','languages']
   useEffect(()=>{
     getUpdatedPreferences()
   },[])
@@ -19,16 +18,49 @@ function RecruiterPreference({currentUser}) {
     setJobDetails(data)
     setPreferences(data.preferences)
   }
+
+  function handleOnDragEnd(result){
+    if(!result.destination) return;
+    const items = Array.from(preferences)
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    setPreferences(items)
+    console.log(preferences);
+  }
+
   function renderPreferences(){
-    return preferences.map(category=>{
-       return <div key={category}>{category}</div>
-    })
+    return(
+      <div>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="preferences">
+            {(provided)=>(
+              <ul {...provided.droppableProps} ref={provided.innerRef}>
+              {preferences.map((category,idx)=>{
+                return(
+                  <Draggable key={category} draggableId={category} index={idx}>
+                  {(provided)=>(
+                    <li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                     {category}
+                    </li>
+                  )}  
+                </Draggable>
+                ) 
+              })}
+              {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
+      )
   }
 
   return ( 
     <>
      <div>Preference page for {jobDetails && jobDetails.company}</div>
-     {renderPreferences()}
+
+       {renderPreferences()}
+
     </>
    );
 }
