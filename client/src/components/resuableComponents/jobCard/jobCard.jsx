4448422d55dc7014fd7978ca0371fc99c0c8cdfc
SKLC1 <segevklein2000@gmail.com>
2022-07-13 17:09:37 +0300

    styling

--- client/src/components/resuableComponents/jobCard/jobCard.jsx
+++ client/src/components/resuableComponents/jobCard/jobCard.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import TinderCard from 'react-tinder-card'
 import { JustFlexRowCV } from '../../../StyledComponents/JustFlexRow';
+import { Card } from '../../../StyledComponents/Card.style'
 import './jobCard.css'
 
 // const URL = process.env?"heroku":"local";
@@ -30,13 +31,19 @@ function JobCard({card, currentUser}){
     console.log(data);
   }
   function renderJobCard(){
-    console.log(card);
-    return card.map(category=>{
+    const displayArr = ['company','role_title','job_description','job_requirements','location'];
+    const cardArr = [];
+    for (const [key, value] of Object.entries(card)) {
+      if (displayArr.includes(key)){
+        cardArr.push([key, value])
+      }
+    }
+    return cardArr.map(categoryObj=>{
       return(
-       <div key={category} >
+       <div key={categoryObj[0]} >
          <JustFlexRowCV>
-         <h4>{category !== 'name' &&`${category}: `}</h4> 
-         {category === 'name'? <h2>{card[category]}</h2>:<p>{card[category]}</p>} 
+          {categoryObj[0] === 'company'?<h2>{categoryObj[1]}</h2>:<h4>{categoryObj[1]}</h4>}
+         {/* <h4>{categoryObj[1]}</h4>  */}
          </JustFlexRowCV>
        </div>
       )
@@ -51,7 +58,9 @@ function JobCard({card, currentUser}){
     onSwipe={onSwipe}
     preventSwipe={['up','down']}>
       <div className='job-card'>
-       {renderJobCard}
+       <Card>
+        {renderJobCard()}
+       </Card>
       </div>
     </TinderCard>
       </div>
