c8001412bbd233c18bf917ff65da63bd06c25ed1
SKLC1 <segevklein2000@gmail.com>
2022-07-14 11:54:56 +0300

    a

--- server/server.js
+++ server/server.js
@@ -32,6 +32,13 @@ app.use('/users', usersRouter)
 app.use('/hr_users', hrUsersRouter)
 app.use('/jobs', jobsRouter)
 
+if(process.env.NODE_ENV){
+  app.use(express.static('/client/build'))
+
+  app.get('*',(req ,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', "index.html"))
+  })
+}
 //server
 const server = http.createServer(app)
 
@@ -68,13 +75,6 @@ io.on("connection", (socket)=>{
   })
 });
 
-if(process.env.NODE_ENV){
-  app.use(express.static('/client/build'))
-
-  app.get('*',(req ,res)=>{
-    res.sendFile(path.resolve(__dirname, 'client', 'build', "index.html"))
-  })
-}
 
 server.listen(PORT,()=>{
   console.log(`server running on ${PORT}`);
