ee43f83418a0cb18d58a13c5ceadff4b0b5669ca
SKLC1 <segevklein2000@gmail.com>
2022-07-14 12:01:04 +0300

    a

--- server/server.js
+++ server/server.js
@@ -33,7 +33,8 @@ app.use('/hr_users', hrUsersRouter)
 app.use('/jobs', jobsRouter)
 
 if(process.env.NODE_ENV){
-  app.use(express.static('/client/build'))
+  
+  app.use(express.static(__dirname,'/client/build'))
 
   app.get('*',(req ,res)=>{
     res.sendFile(path.resolve(__dirname, 'client', 'build', "index.html"))
