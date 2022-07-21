be4314b96c9231ceedc7addf96e28245748603a0
SKLC1 <segevklein2000@gmail.com>
2022-07-21 15:18:48 +0300

    style

--- client/src/StyledComponents/InputAndLabel.style.jsx
+++ client/src/StyledComponents/InputAndLabel.style.jsx
@@ -2,9 +2,9 @@ import styled from "@emotion/styled"
 
 export const InputAndLabel = styled.div`
 
-  & > * {
+  & > div > * {
     font-weight: 500;
     color: hsl(210,99%,50%);
-    margin: 0.3rem;
+    margin: 0.4rem;
   }
 `
