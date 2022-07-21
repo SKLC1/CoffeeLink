1e22f8f24554c57f5de15b9778f383a7e9e89a4e
SKLC1 <segevklein2000@gmail.com>
2022-07-21 15:06:04 +0300

    style login

--- client/src/StyledComponents/Form.style.jsx
+++ client/src/StyledComponents/Form.style.jsx
@@ -3,16 +3,24 @@ import styled from "@emotion/styled"
 
 export const Form = styled.div`
 padding: 1rem;
-margin: 3rem;
+/* margin: rem; */
 box-shadow: 0 2px 10px 0 rgba(0,0,0,.2);
 border-radius: 20px;
 background-color: #fff;
+margin: auto;
+margin-top: 3rem;
+
+
 
 display: flex;
 flex-direction: column;
 align-items: center;
 justify-content: center;
 
+
+@media (min-width: 900px) {
+  max-width: 60%;
+}
 & > div > label {
   color: hsl(210,99%,50%);
   }
--- client/src/StyledComponents/flexCustom.jsx
+++ client/src/StyledComponents/flexCustom.jsx
@@ -6,4 +6,5 @@ flex-direction: ${(props)=> props.direction };
 align-items: ${(props)=> props.align };
 justify-content: ${(props)=> props.justify };
 height: ${(props)=> props.height};
+
 `
